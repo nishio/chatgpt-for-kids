4aa47d0d412e400dcc3def3d11ec468456ac5e21
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-28 00:05:50 +0900

    Prettier

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -1,41 +1,44 @@
-import { createParser } from 'eventsource-parser'
-import type { ParsedEvent, ReconnectInterval } from 'eventsource-parser'
-import type { ChatMessage } from '@/types'
+import { createParser } from "eventsource-parser";
+import type { ParsedEvent, ReconnectInterval } from "eventsource-parser";
+import type { ChatMessage } from "@/types";
 
-const model = import.meta.env.OPENAI_API_MODEL || 'gpt-3.5-turbo'
+const model = import.meta.env.OPENAI_API_MODEL || "gpt-3.5-turbo";
 
-export const generatePayload = (apiKey: string, messages: ChatMessage[]): RequestInit & { dispatcher?: any } => ({
+export const generatePayload = (
+  apiKey: string,
+  messages: ChatMessage[]
+): RequestInit & { dispatcher?: any } => ({
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`,
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
   },
-  method: 'POST',
+  method: "POST",
   body: JSON.stringify({
     model,
     messages,
     temperature: 0.6,
     stream: true,
   }),
-})
+});
 
 export const parseOpenAIStream = (rawResponse: Response) => {
-  const encoder = new TextEncoder()
-  const decoder = new TextDecoder()
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
   if (!rawResponse.ok) {
     return new Response(rawResponse.body, {
       status: rawResponse.status,
       statusText: rawResponse.statusText,
-    })
+    });
   }
 
   const stream = new ReadableStream({
     async start(controller) {
       const streamParser = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data
-          if (data === '[DONE]') {
-            controller.close()
-            return
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            controller.close();
+            return;
           }
           try {
             // response = {
@@ -47,21 +50,21 @@ export const parseOpenAIStream = (rawResponse: Response) => {
             //     { delta: { content: '你' }, index: 0, finish_reason: null }
             //   ],
             // }
-            const json = JSON.parse(data)
-            const text = json.choices[0].delta?.content || ''
-            const queue = encoder.encode(text)
-            controller.enqueue(queue)
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta?.content || "";
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
           } catch (e) {
-            controller.error(e)
+            controller.error(e);
           }
         }
-      }
+      };
 
-      const parser = createParser(streamParser)
+      const parser = createParser(streamParser);
       for await (const chunk of rawResponse.body as any)
-        parser.feed(decoder.decode(chunk))
+        parser.feed(decoder.decode(chunk));
     },
-  })
+  });
 
-  return new Response(stream)
-}
+  return new Response(stream);
+};
