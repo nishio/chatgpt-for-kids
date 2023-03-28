be3230c9e7fa1144108e499089f97a698f3892fe
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-28 15:37:33 +0900

    Prettier

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 export interface ChatMessage {
-  role: 'system' | 'user' | 'assistant'
-  content: string
+  role: "system" | "user" | "assistant";
+  content: string;
 }
 
 export interface ErrorMessage {
-  code: string
-  message: string
+  code: string;
+  message: string;
 }
