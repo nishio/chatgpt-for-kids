8cda85a556ed7d6e183ab107c6105ef9137add85
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-29 01:13:43 +0900

    remove

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -2,7 +2,7 @@ import { createParser } from "eventsource-parser";
 import type { ParsedEvent, ReconnectInterval } from "eventsource-parser";
 import type { ChatMessage } from "@/types";
 
-const model = import.meta.env.OPENAI_API_MODEL || "gpt-3.5-turbo";
+const model = import.meta.env.OPENAI_API_MODEL;
 
 export const generatePayload = (
   apiKey: string,
@@ -41,15 +41,6 @@ export const parseOpenAIStream = (rawResponse: Response) => {
             return;
           }
           try {
-            // response = {
-            //   id: 'chatcmpl-6pULPSegWhFgi0XQ1DtgA3zTa1WR6',
-            //   object: 'chat.completion.chunk',
-            //   created: 1677729391,
-            //   model: 'gpt-3.5-turbo-0301',
-            //   choices: [
-            //     { delta: { content: '你' }, index: 0, finish_reason: null }
-            //   ],
-            // }
             const json = JSON.parse(data);
             const text = json.choices[0].delta?.content || "";
             const queue = encoder.encode(text);
