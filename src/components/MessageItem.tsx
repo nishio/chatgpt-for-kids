86d53188a318059991d136f481af0f0ff6b34e08
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-03-30 22:17:58 +0900

    add checkbox (not used yet)

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -73,6 +73,9 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
   return (
     <div class="py-2 -mx-4 px-4 transition-colors md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === "user"}>
+        <div class="w-7 h-7 mt-4">
+          <input type="checkbox" checked />
+        </div>
         <div
           class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`}
         />
@@ -80,7 +83,6 @@ export default ({ role, message, showRetry, onRetry }: Props) => {
           class="message prose break-words overflow-hidden"
           innerHTML={htmlString()}
         />
-        {/* <input type="checkbox">使う</input> */}
       </div>
       {showRetry?.() && onRetry && (
         <div class="fie px-3 mb-2">
